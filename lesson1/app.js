c435fabda699f5782d45bea735b0a09b1c8fe964
RomanHrytsko <romangritsko@gmail.com>
2021-02-19 15:18:34 +0200

    homework completed

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,103 +1,74 @@
-const x = require('./dir/file')
-
-
-// x.createUser('Roman', 22)
-
-// console.log('*****************************8')
-// console.log(__dirname)
-// console.log(__filename)
-// console.log(global)
-// console.log('*****************************8')
-
-
-// console.log(process)
-
-//USE FS TO WORK WITH FILES
-
 const fs = require('fs')
-const path = require('path')
 
-const filePath = __dirname + '/netflix/king.mp4'
-const dirName = __dirname + '/dir'
-//CREATE FILE AND NOTE DATA
-// fs.writeFile(filePath, 'Hello sep-2021', err => {
+
+const dirName1800 = `${__dirname}/1800`
+const dirName2000 = `${__dirname}/2000`
+
+// Task
+// CREATE 2 DIRECTORIES 1800 AND 2000
+// EACH DIRECTORY NEED CONTAIN MEN AND WOMEN
+// SORT WOMEN TO 2000 AND MEN TO 1800
+
+//CREATING DIRECTORIES
+// fs.mkdir(`${__dirname}/2000`, err => {
 //     if (err) {
 //         console.log(err)
 //     }
 // })
 
-//UPDATE FILE
-// fs.appendFile(filePath, 'Hello \n', err => {
-//     if(err){
+// fs.mkdir(`${__dirname}/1800`, err => {
+//     if (err) {
 //         console.log(err)
 //     }
 // })
 
-//CREATE DIRECTORY
-// fs.mkdir(`${__dirname}/netflix/films/chubaka`, {recursive: true}, err => {
-//     if(err){
+//CREATING FILES
+
+// fs.writeFile(`${dirName2000}/Bogdan.json`, '{name: "Bogdan", gender: "male"}' , err => {
+//     if (err) {
 //         console.log(err)
 //     }
 // })
 
-// fs.readFile(filePath, (err, data) => {
-//     console.log(data.toString())
-// })
+//READING FILES AND SORTING
 
-//REMOVE DIRECTORY
-// fs.rmdir(`${__dirname}/netflix`, {recursive: true}, err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
+fs.readdir(dirName2000, ((err, files) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    files.forEach(fileName => {
+        fs.readFile(dirName2000 + `/${fileName}`, ((err1, stats) => {
+            if (err1) {
+                console.log(err)
+                return
+            }
+            let person = JSON.parse(stats)
+            if (person.gender === 'male') {
+                fs.rename(dirName2000 + `/${fileName}`, dirName1800 + `/${fileName}`, err2 => {
+                    if (err2) {
+                        console.log(err2)
+                    }
+                })
+            }
 
-//READ FILE
-// fs.readdir(dirName, ((err, files) =>{
-//     if(err){
-//         console.log(err)
-//         return
-//     }
-//     files.forEach(filename => {
-//         fs.stat(dirName + `/${filename}`, ((err1, stats) => {
-//             console.log('--------------------------------------')
-//                 //INFO ABOUT FILE
-//             console.log(stats.isDirectory())
-//             console.log('--------------------------------------')
-//         }))
-//     })
-//
-// } ))
+        }))
+    })
+}))
+fs.readdir(dirName1800, ((err, files) => {
+        if (err) {
+            console.log(err)
+        }
+        files.forEach(fileName => {
+            fs.readFile(dirName1800 + `/${fileName}`, (err1, data) => {
+                let person = JSON.parse(data)
+                if (person.gender === 'female') {
+                    fs.rename(dirName1800 + `/${fileName}`, dirName2000 + `/${fileName}`, err2 => {
+                        console.log(err2)
+                    })
+                }
+            })
+        })
+    })
+)
 
-//DELETE FILE BY PASS
-// fs.unlink(filePath, err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-//RENAME OR MOVE FILES
-// fs.rename(`${__dirname}/dir/file2.txt`,`${__dirname}/netflix/king.mp4` , err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-
-// let s = path.join(__dirname,'dir','file.txt')
-
-
-//STREAM
-// const readStream = fs.createReadStream(filePath);
-// readStream.on('data', chunk => {
-//     console.log(chunk)
-// })
-
-const readStream = fs.createReadStream(path.join(__dirname, 'dir', 'king.txt'))
-const writeStream = fs.createWriteStream(path.join(__dirname, 'dir', 'data.txt'))
-
-//
-// readStream.on('data', chunk => {
-//     writeStream.write(chunk)
-// })
-
-readStream.pipe(writeStream)
