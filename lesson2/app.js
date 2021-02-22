b5362696264281b72fd3a9e06281dfa8fcfcd2ad
RomanHrytsko <romangritsko@gmail.com>
2021-02-23 00:07:05 +0200

    fc

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,103 +1,73 @@
-const x = require('../junk/dir/file')
-
-
-// x.createUser('Roman', 22)
-
-// console.log('*****************************8')
-// console.log(__dirname)
-// console.log(__filename)
-// console.log(global)
-// console.log('*****************************8')
-
-
-// console.log(process)
-
-//USE FS TO WORK WITH FILES
-
-const fs = require('fs')
+const express = require('express')
+const expressHBS = require('express-handlebars')
 const path = require('path')
 
-const filePath = __dirname + '/netflix/king.mp4'
-const dirName = __dirname + '/dir'
-//CREATE FILE AND NOTE DATA
-// fs.writeFile(filePath, 'Hello sep-2021', err => {
-//     if (err) {
-//         console.log(err)
-//     }
-// })
-
-//UPDATE FILE
-// fs.appendFile(filePath, 'Hello \n', err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-//CREATE DIRECTORY
-// fs.mkdir(`${__dirname}/netflix/films/chubaka`, {recursive: true}, err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-// fs.readFile(filePath, (err, data) => {
-//     console.log(data.toString())
-// })
-
-//REMOVE DIRECTORY
-// fs.rmdir(`${__dirname}/netflix`, {recursive: true}, err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-//READ FILE
-// fs.readdir(dirName, ((err, files) =>{
-//     if(err){
-//         console.log(err)
-//         return
-//     }
-//     files.forEach(filename => {
-//         fs.stat(dirName + `/${filename}`, ((err1, stats) => {
-//             console.log('--------------------------------------')
-//                 //INFO ABOUT FILE
-//             console.log(stats.isDirectory())
-//             console.log('--------------------------------------')
-//         }))
-//     })
-//
-// } ))
-
-//DELETE FILE BY PASS
-// fs.unlink(filePath, err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
-
-//RENAME OR MOVE FILES
-// fs.rename(`${__dirname}/dir/file2.txt`,`${__dirname}/netflix/king.mp4` , err => {
-//     if(err){
-//         console.log(err)
-//     }
-// })
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
-// let s = path.join(__dirname,'dir','file.txt')
+//HBS CONFIG START----------------
+app.use(express.static(path.join(__dirname, 'static'))) //static directory
+
+app.set('view engine', '.hbs')
+app.engine('.hbs', expressHBS({
+    defaultLayout: false
+}))
+app.set('views', path.join(__dirname, 'static'))
+//HBS CONFIG END----------------
+
+const users = [
+    {name: 'Dima', gender: 'male'},
+    {name: 'Dido', gender: 'male'},
+    {name: 'Diana', gender: 'female'},
+]
 
 
-//STREAM
-// const readStream = fs.createReadStream(filePath);
-// readStream.on('data', chunk => {
-//     console.log(chunk)
-// })
+//HTTP METHODS
 
-const readStream = fs.createReadStream(path.join(__dirname, 'dir', 'king.txt'))
-const writeStream = fs.createWriteStream(path.join(__dirname, 'dir', 'data.txt'))
 
-//
-// readStream.on('data', chunk => {
-//     writeStream.write(chunk)
-// })
+//GET
+app.get('/hello', (req, res) => {
+    //res.send contains write + end
+    //res.send('WORLD')
+//--------------------------------
+    // res.write always waiting for request, so that's why we use res.end,
 
-readStream.pipe(writeStream)
+    // res.write('WORLD')
+    // res.end()//--------------------------------
+
+})
+
+app.get('/login', (req, res) => {
+    res.render('login', {isOk: true, name: "Andrey"})
+})
+app.post('/login', ((req, res) => {
+
+    users.push(req.body)
+    // res.json('User registered')
+    res.redirect('/users')
+
+}))
+
+
+app.get('/users', (req, res) => {
+
+    const {gender} = req.query
+    const filterUsers = users.filter(value => value.gender === gender)
+    res.render('users', {users: filterUsers})
+    // res.json([
+    //     {name: 'Dima'},
+    //     {name: 'Olga'},
+    //     {name: 'Ira'},
+    // ])
+})
+app.get('/users/:userId', (req, res) => {
+
+    const {userId} = req.params
+    res.json(users[userId])
+})
+
+app.listen(5000, () => {
+    console.log('App listen 5000')
+})
